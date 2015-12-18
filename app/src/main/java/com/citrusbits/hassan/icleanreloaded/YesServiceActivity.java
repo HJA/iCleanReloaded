d16bd0638375ff5a2e8ffbbbd3f0e6aa5dc86f8e
HJA <hassan@citrusbits.com>
2015-12-18 19:01:42 +0500

    Added 2 new screens and their respective classes.

--- app/src/main/java/com/citrusbits/hassan/icleanreloaded/YesServiceActivity.java
+++ app/src/main/java/com/citrusbits/hassan/icleanreloaded/YesServiceActivity.java
@@ -1,13 +1,31 @@
 package com.citrusbits.hassan.icleanreloaded;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class YesServiceActivity extends AppCompatActivity {
+
+    ImageButton yesservice_okay_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.yes_service_activity);
+
+        yesservice_okay_btn = (ImageButton) findViewById(R.id.yesservice_okay_btn);
+
+        yesservice_okay_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (v.getId() == R.id.yesservice_okay_btn) {
+                    Intent loggedIn = new Intent(YesServiceActivity.this, AddLocationActivity.class);
+                    startActivity(loggedIn);
+                }
+            }
+        });
     }
 }
